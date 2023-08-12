b54236e99175a371b23c8e29b3a66a100c45bef5
SzymonKonieczny <kowladMC@gmail.com>
2023-08-12 15:54:56 +0200

    texture

---
+++ VoxelGame/Renderer/Texture.cpp
@@ -0,0 +1,34 @@
+#include "Texture.h"
+#include "stb/stb_image.h"
+Texture::Texture(std::string path)
+	:Path(path), m_Width(0), m_Height(0), m_ID(0), m_BitsPerPixel(0), DataBuffer(nullptr)
+{
+	
+	
+	glGenTextures(1, &m_ID);
+	glBindTexture(GL_TEXTURE_2D, m_ID);
+
+	stbi_set_flip_vertically_on_load(1);
+	stbi_load(DataBuffer, &m_Width, &m_Height, &m_BitsPerPixel, 4);
+
+	glTextureStorage2D(m_ID, 1, GL_RGBA8, m_Width, m_Height);
+
+	glTextureParameteri(m_ID, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTextureParameteri(m_ID, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+						
+	glTextureParameteri(m_ID, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTextureParameteri(m_ID, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+	glTextureSubImage2D(m_ID, 0, 0, 0, m_Width, m_Height, GL_RGBA, GL_UNSIGNED_BYTE, DataBuffer);
+
+	stbi_image_free(DataBuffer);
+}
+
+void Texture::Bind(unsigned int slot)
+{
+	glBindTextureUnit(slot,m_ID);
+}
+
+void Texture::Unbind()
+{
+}
---
+++ VoxelGame/Renderer/Texture.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <string>
+#include <glad/glad.h>
+class Texture {
+public:
+	Texture(std::string path);
+
+	void Bind(unsigned int slot = 0);
+	void Unbind();
+
+private:
+	unsigned int m_ID;
+	 char* DataBuffer;
+	int m_Width, m_Height, m_BitsPerPixel;
+	std::string Path;
+
+
+};
